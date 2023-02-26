380145506fa28399d9432ff0e16339b8094e94e8
latronixo1 <latronixo@gmail.com>
2023-02-26 18:33:39 +0300

    Initial Commit

---
+++ 96. Quizzler/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  96. Quizzler
+//
+//  Created by MacBook on 26.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
